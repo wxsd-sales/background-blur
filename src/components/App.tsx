a22eda8eab893ec582fa7753bbe2263783a10d5d
Arash Koushkebaghi <arash.koush@gmail.com>
2021-03-25 19:15:42 -0700

    simplify more components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,40 +1,13 @@
 import React from 'react';
-import {Button, Input, Spinner, Icon} from '@momentum-ui/react';
 import {Inputs, Media} from './';
 
-export default class App extends React.Component<any, any> {
-  webex: any;
-  meeting: any;
-  option: any;
-  canvas: any;
-  localStream: any;
-  remoteStream: any;
-  mediaSettings: any;
-  videoWidth: string;
-  videoHeight: string;
 
-  constructor(props) {
-    super(props);
-  }
-
-  async componentWillUnmount() {
-    try {
-      await this.webex.meetings.unregister();
-      await this.webex.internal.mercury.disconnect();
-      await this.webex.internal.device.unregister();
-
-    } catch (e) {
-      console.log(`Webex Disconnection Error: ${e}`);
-    }
-  }
-
-  render() {
-    return (
-      <div className="content">
-        <h2>Blur Background Webex Meeting Feature</h2>
-        <Inputs />
-        <Media />
-      </div> 
-    );
-  }
+export default() => {
+  return (
+    <div className="content">
+      <h2>Blur Background Webex Meeting Feature</h2>
+      <Inputs />
+      <Media />
+    </div> 
+  );
 }
