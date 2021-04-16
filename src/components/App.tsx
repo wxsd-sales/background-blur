0d4fcb9799bd3316432ce4dcca12f16a314550f5
Arash Koushkebaghi <arash.koush@gmail.com>
2021-04-15 18:06:38 -0700

    add github link to demo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import {Inputs, Media} from './';
+import {Button, Icon} from '@momentum-ui/react';
 
 
 export default() => {
+  const githubPage = 'https://github.com/WXSD-Sales/BlurBackground';
   return (
     <div className="content">
+      <div className="header">
       <h2>Webex Browser SDK - Background Blur using TensorflowJS</h2>
+      <Button circle color="blue" onClick={() => {window.open(githubPage, '_blank')}>
+          <Icon name="info_18" />  
+        </Button >
+      </div>
       <Inputs />
       <Media />
     </div> 
